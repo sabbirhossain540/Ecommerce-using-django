1519b7cc4702c3c13b3db7ba07f9034b3991349e
sabbirhossain540 <m.sabbirhossain308@gmail.com>
2020-04-26 15:52:16 +0600

    Day 16

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -18,6 +18,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index),
+    path('',views.index , name="home"),
+    path('about/',views.about , name="about"),
+    path('contact/',views.contact , name="contact"),
     path('admin/', admin.site.urls),
 ]
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -2,7 +2,25 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "home_page.html", {})
+    context = {
+        "title" : "Index Page",
+        "content" : "Welcome To the Home page"
+    }
+    return render(request, "home_page.html", context)
+
+def about(request):
+    context = {
+        "title" : "About Page",
+        "content" : "Welcome To the About page"
+    }
+    return render(request, "home_page.html", context)
+
+def contact(request):
+    context = {
+        "title" : "Contact Page",
+        "content" : "Welcome To the Contact page"
+    }
+    return render(request, "home_page.html", context)
 
 
 
