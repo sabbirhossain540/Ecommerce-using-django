b1fdcd0483616e9fec43c2ba794d26ef55f20d26
sabbirhossain540 <m.sabbirhossain308@gmail.com>
2020-04-27 23:18:17 +0600

    Day 17

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -117,4 +117,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+#Static File Setup Start
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'ecommerce/static')
+]
+
+
+#------------- End Static File Area -----------------#
+
+#Media File Setup Start
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "static_cdn", "media_root")
+
+
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -13,6 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
 from django.urls import path
 from . import views
@@ -25,3 +28,7 @@ urlpatterns = [
     path('register/',views.register_page , name="register"),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
