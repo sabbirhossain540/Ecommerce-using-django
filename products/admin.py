b08f0ca7c897e2a2780f4faaa1bbc733d31a14ce
sabbirhossain540 <m.sabbirhossain308@gmail.com>
2020-04-28 16:30:05 +0600

    Day 18

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Product
+
+
+#Register Model Into Admin Site
+admin.site.register(Product)
--- products/models.py
+++ products/models.py
@@ -6,3 +6,13 @@ class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=20, default=39.99)
+
+    def __str__(self):
+        return self.title
+    
+
+    #Use For Python 2 Specially
+    def __unicode__(self):
+        return self.title
+
+
