09b5b23878d5556bcb31b1e621665bf66ec5d676
sabbirhossain540 <m.sabbirhossain308@gmail.com>
2020-05-02 17:10:58 +0600

    Day 21

--- products/models.py
+++ products/models.py
@@ -45,7 +45,7 @@ class ProductManager(models.Manager):
         return self.get_queryset().filter(active=True)
     
     def search(self, query):
-        lookups = Q(title__icontains=query) | Q(description__icontains=query) | Q(price__icontains=query)
+        lookups = Q(title__icontains=query) | Q(description__icontains=query) | Q(price__icontains=query) | Q(tag__title__icontains=query) | Q(tag__slug__icontains=query)
         return self.filter(lookups).distinct()
 
 
