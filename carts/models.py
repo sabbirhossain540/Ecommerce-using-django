f1c86b66558b647c12c3e5fcd95236e504eba666
sabbirhossain540 <m.sabbirhossain308@gmail.com>
2020-05-03 16:52:21 +0600

    Day 22

--- carts/models.py
+++ carts/models.py
@@ -1,4 +1,17 @@
 from django.db import models
+from django.conf import settings
+from products.models import Product
+
+User = settings.AUTH_USER_MODEL
 
 # Create your models here.
 
+class Cart(models.Model):
+    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product, blank=True)
+    total = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
+    updated = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+def __str__(self):
+    return str(self.id)
--- carts/views.py
+++ carts/views.py
@@ -3,11 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 
 def cart_home(request):
-    print(request.session)
-    #print(dir(request.session))
-    key = request.session.session_key
-    print(key)
-    #request.session.set_expiry(300)
+    card_id = request.session.get("cart_id", None)
+
+    if card_id is None:
+        print('Create New Cart')
+        request.session['cart_id'] = 13
+    else:
+        print("Card is Exits")
 
 
     return render(request, "carts/home.html")
