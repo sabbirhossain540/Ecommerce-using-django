9a02dba6a74ea5dd979691cb717a8d34db572ac3
sabbirhossain540 <m.sabbirhossain308@gmail.com>
2020-05-03 16:32:47 +0600

    Day 22

--- carts/views.py
+++ carts/views.py
@@ -3,4 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 
 def cart_home(request):
+    print(request.session)
+    #print(dir(request.session))
+    key = request.session.session_key
+    print(key)
+    #request.session.set_expiry(300)
+
+
     return render(request, "carts/home.html")
