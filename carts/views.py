77e5b1ddd8d317f76d01a91fbc7d25caf50d9300
sabbirhossain540 <m.sabbirhossain308@gmail.com>
2020-05-04 11:40:19 +0600

    Day 23

--- carts/views.py
+++ carts/views.py
@@ -1,15 +1,31 @@
 from django.shortcuts import render
+from .models import Cart
 
 # Create your views here.
 
-def cart_home(request):
-    card_id = request.session.get("cart_id", None)
+def cart_create(user=None):
+    cart_obj = Cart.objects.create(user=None)
+    print('New Cart Created')
+    return cart_obj
 
-    if card_id is None:
-        print('Create New Cart')
-        request.session['cart_id'] = 13
+def cart_home(request):
+
+    request.session['cart_id'] = "12"
+    cart_id = request.session.get("cart_id", None)
+    
+    # if cart_id is None:
+        
+    #     cart_obj = cart_create()
+    #     request.session['cart_id'] = cart_obj
+    #     print('New Cart Created')
+    # else:
+    qs = Cart.objects.filter(id=cart_id)
+    if qs.count() == 1:
+      print("Cart Id exists")
+      cart_obj = qs.first()
     else:
-        print("Card is Exits")
+      cart_obj = cart_create()
+      request.session['cart_id'] = cart_obj
 
 
     return render(request, "carts/home.html")
