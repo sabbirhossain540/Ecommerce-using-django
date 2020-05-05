cfd6ddd0195e7e5864dcf210407a5d92801d763d
sabbirhossain540 <m.sabbirhossain308@gmail.com>
2020-05-05 16:52:34 +0600

    Day 24

--- carts/urls.py
+++ carts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import cart_home
+from .views import cart_home, cart_update
 
 urlpatterns = [
    path('', cart_home , name="cart"),
+   path('cart_update/', cart_update, 'cart_update'),
 ]
--- carts/views.py
+++ carts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Cart
 from django.core import serializers
+from products.models import Product
 
 # Create your views here.
 
@@ -23,7 +24,25 @@ def cart_home(request):
     #   #cart_obj = cart_create()
     #   cart_obj = Cart.objects.new(user=request.user)
     #   request.session['cart_id'] = cart_obj.id
-    cart_obj = Cart.objects.new_or_get(request)
-
+    cart_obj, new_obj = Cart.objects.new_or_get(request)
+    products = cart_obj.products.all()
+    total = 0
+    for x in products:
+      total += x.price
+    print(total)
+    cart_obj.total = total
+    cart_obj.save()
 
     return render(request, "carts/home.html")
+
+def cart_update(request):
+  product_id = 1
+  product_obj = Product.objects.get(id=product_id)
+  cart_obj, new_obj = Cart.objects.new_or_get(request)
+
+  if product_obj in cart_obj.products.all():
+    cart_obj.products.remove(product_obj)
+  else:
+    cart_obj.products.add(product_obj)
+  
+  return redirect("cart")
